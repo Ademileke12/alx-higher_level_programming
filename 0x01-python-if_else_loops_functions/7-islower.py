831b8496d5d6df5167c7dab47594fc9a0cca6ee2
Ademileke12 <ademilekeabudu@gail.com>
2022-08-24 16:40:58 -0700

    python if else and loop

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,25 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    return c.islower()
-
-
-if islower('a') is True:
-    print("a is lower")
-else:
-    print("a is upper")
-if islower('H') is True:
-    print("H is lower")
-else:
-    print("H is upper")
-if islower('A') is True:
-    print("A is lower")
-else:
-    print("A is upper")
-if islower('3') is True:
-    print("3 is lower")
-else:
-    print("3 is upper")
-if islower('g') is True:
-    print("g is lower")
-else:
-    print("g is upper")
+    if ord(c) > 96 and ord(c) < 123:
+        return True
+    return False
