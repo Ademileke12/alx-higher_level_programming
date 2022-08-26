6e617b78adbb9933c99fc6944da19348697e64ed
Ademileke12 <ademilekeabudu@gail.com>
2022-08-25 17:34:17 -0700

    Python modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,13 @@ if __name__ == "__main__":
     import sys
     lenthT = len(sys.argv)
     if lenthT <= 1:
-        print("0 arguments.")
+        print("{} arguments.".format(lenthT - 1))
     else:
-        if lenthT == 1:
-            print("{} arguments:".format(lenthT))
-        else:
+        if lenthT == 2:
             print("{} argument:".format(lenthT - 1))
+            for i in range(1, lenthT):
+                print("{}: {}".format(i, sys.argv[i]))
+        else:
+            print("{} arguments:".format(lenthT - 1))
             for i in range(1, lenthT):
                 print("{}: {}".format(i, sys.argv[i]))
