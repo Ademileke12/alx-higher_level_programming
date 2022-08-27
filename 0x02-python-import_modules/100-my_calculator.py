220c077c7d936d7fd8223585bc5a4c8e84ad8ac2
Ademileke <38130432+Ademileke12@users.noreply.github.com>
2022-08-27 22:57:22 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,8 +5,7 @@ if __name__ == "__main__":
     arg = len(argv)
     if arg >= 4:
         if argv[2] == '+':
-            print("{} + {} = {}".format(argv[1], argv[3], add(int(argv[1]), 
-            int(argv[3]))))
+            print("{} + {} = {}".format(argv[1], argv[3], add(int(argv[1]),int(argv[3]))))
         elif argv[2] == '-':
             print("{} - {} = {}".format(argv[1], argv[3], sub(int(argv[1]), 
             int(argv[3]))))
