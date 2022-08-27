1c4ceb8414edc5631dc3cd8f1022321e6481a1ee
Ademileke <38130432+Ademileke12@users.noreply.github.com>
2022-08-27 22:55:44 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-if __name__ ==  "__main__":
+if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
     arg = len(argv)
     if arg >= 4:
         if argv[2] == '+':
-            print("{} + {} = {}".format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
+            print("{} + {} = {}".format(argv[1], argv[3], add(int(argv[1]), 
+            int(argv[3]))))
         elif argv[2] == '-':
-            print("{} - {} = {}".format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
+            print("{} - {} = {}".format(argv[1], argv[3], sub(int(argv[1]), 
+            int(argv[3]))))
         elif argv[2] == '*':
-            print("{} * {} = {}".format(argv[1], argv[3] ,mul(int(argv[1]), int(argv[3]))))
+            print("{} * {} = {}".format(argv[1], argv[3], mul(int(argv[1]), 
+            int(argv[3]))))
         elif argv[2] == '/':
-            print("{} / {} = {}".format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
+            print("{} / {} = {}".format(argv[1], argv[3], div(int(argv[1]), 
+            int(argv[3]))))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
     else:
