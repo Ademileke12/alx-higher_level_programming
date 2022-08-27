8d0b424244fcadaad05fd1d504c6c42dea7d17c8
Ademileke12 <ademilekeabudu@gail.com>
2022-08-27 15:21:10 -0700

    python module

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,19 +3,16 @@ if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
     arg = len(argv)
-    if arg >= 4:
+    a = int(argv[1])
+    b = int(argv[3])
+    if arg >= 3:
         if argv[2] == '+':
-            print("{} + {} = {}".format(argv[1], argv[3], add(int(argv[1]),int(argv[3]))))
+            print("{} + {} = {}".format(a, b, add(a, b)))
         elif argv[2] == '-':
-            print("{} - {} = {}".format(argv[1], argv[3], sub(int(argv[1]), 
-            int(argv[3]))))
+            print("{} - {} = {}".format(a, b, sub(a, b)))
         elif argv[2] == '*':
-            print("{} * {} = {}".format(argv[1], argv[3], mul(int(argv[1]), 
-            int(argv[3]))))
+            print("{} * {} = {}".format(a, b, mul(a, b)))
         elif argv[2] == '/':
-            print("{} / {} = {}".format(argv[1], argv[3], div(int(argv[1]), 
-            int(argv[3]))))
+            print("{} / {} = {}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-    else:
-        print(len(argv))
