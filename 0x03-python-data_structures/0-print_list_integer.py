4088b36369a865beb83b03885ca40d1612aba061
Ademileke12 <ademilekeabudu@gail.com>
2022-08-29 04:36:37 -0700

    py data

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    """Print list of integers
-    Args:
-    size: length of list
-    i: loop through list
-    Returns:
-     return nothing but prints my_list on a new line
-    """
-    size = len(my_list)
-    for i in my_list:
-        print("{}".format(i))
+    for n in my_list:
+        print("{:d}".format(n))
