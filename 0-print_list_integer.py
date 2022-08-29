8f7d50df857f967f5609b16f45fec85945083135
Ademileke <38130432+Ademileke12@users.noreply.github.com>
2022-08-29 10:01:41 +0100

    Delete 0-print_list_integer.py

--- 0-print_list_integer.py
+++
@@ -1,7 +0,0 @@
-#!usr/bin/python3
-def print_list_integer(my_list=[]):
-    return my_list
-my_list = [1, 2, 3, 4, 5]
-size = len(my_list)
-for i in range(0 ,size):
-    print("{}".format(print_list_integer(my_list[i])))
