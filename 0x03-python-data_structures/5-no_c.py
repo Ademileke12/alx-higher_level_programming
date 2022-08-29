d1d58051611f81a992810e2796f6d19ba9a2bd2d
Ademileke12 <ademilekeabudu@gail.com>
2022-08-29 12:50:59 -0700

    remove cC

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if ('C' not in my_string) and ('c' not in my_string):
+        return
+    else:
+        my_string = my_string.translate({ord(i): None for i in 'C'})
+        my_string = my_string.translate({ord(i): None for i in 'c'})
+    return my_string
