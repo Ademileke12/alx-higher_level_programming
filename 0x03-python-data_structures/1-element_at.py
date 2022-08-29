15905c0e70865c49765fce05a698b398f16e05bc
Ademileke12 <ademilekeabudu@gail.com>
2022-08-29 06:32:12 -0700

    find index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     size = len(my_list)
-    if idx < 0 or idx > size:
+    if idx < 0:
+        return "none"
+    elif idx > size:
         return "none"
     elif idx >= 0 <= size:
         return my_list[idx]
