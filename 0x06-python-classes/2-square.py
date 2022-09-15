f9651fcffd6205b3d14d194a874c96f768939c1c
Ademileke12 <ademilekeabudu@gail.com>
2022-09-14 23:42:25 -0700

    square2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This module is the square class
+"""
+
+class Square:
+    """The class implementation"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+             raise ValueError("size must be >=0")
