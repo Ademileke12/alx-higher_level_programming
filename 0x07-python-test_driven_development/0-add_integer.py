5d5072c1fd08a4e260eecc888211ed9e3fcfdba4
Ademileke <38130432+Ademileke12@users.noreply.github.com>
2022-09-21 17:21:25 +0100

    Delete 0x07-python-test_driven_development directory

--- 0x07-python-test_driven_development/0-add_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def add_integer(a, b=98):
-    """ returns the addition of two integers """
-    if type(a) != int and type(a) != float:
-        raise TypeError('a must be an integer')
-    elif type(b) != int and type(b) != float:
-        raise TypeError('b must be an integer')
-    else:
-        return (int(a) + int(b))
--- 0x07-python-test_driven_development/3-say_my_name.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def say_my_name(first_name, last_name=""):
-    if type(first_name) != str:
-        raise TypeError('first_name must be a string')
-    else:
-        print("My name is {} {}".format(first_name, last_name))
